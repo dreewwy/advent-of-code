1fb78ebe0acf9e738c8ce3ae34ceb6a738d2b15a
Andrew <depetra@mcmaster.ca>
2024-12-02 03:21:45 -0500

    initial commit

---
+++ day1/target/debug/day1.d
@@ -0,0 +1,1 @@
+D:\Personal\ Projects\advent\ of\ code\ 2024\advent-of-code\day1\target\debug\day1.exe: D:\Personal\ Projects\advent\ of\ code\ 2024\advent-of-code\day1\src\main.rs
---
+++ day1/target/debug/deps/day1.d
@@ -0,0 +1,5 @@
+D:\Personal Projects\advent of code 2024\advent-of-code\day1\target\debug\deps\day1.exe: src/main.rs
+
+D:\Personal Projects\advent of code 2024\advent-of-code\day1\target\debug\deps\day1.d: src/main.rs
+
+src/main.rs:
